ce4f3af410e12fc06d4ec06c9c172fa81d4514ba
chrisLi <m18833368330@163.com>
2020-04-02 20:46:09 +0800

    init

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,9 +4,8 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.printf("llll");
-
-
         // write your code here
+        System.out.println("这咋回事啊");
     }
 
 
